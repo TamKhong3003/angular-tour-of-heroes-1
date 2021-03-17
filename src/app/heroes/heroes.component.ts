2263b3cf08301eb3a5436016a1583e47830346e8
Dang Chuong Pham <dangchuong1999@outlook.com>
2021-03-17 22:06:40 +0200

    feat heroes.component: Used mock-heroes for data.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Hero } from 'src/app/hero'
+import { Hero } from 'src/app/hero';
+import { HEROES } from 'src/app/mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +9,7 @@ import { Hero } from 'src/app/hero'
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes: Hero[] = HEROES;
 
   constructor() { }
 
