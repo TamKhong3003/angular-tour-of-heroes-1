68004f4800382b5a25c7b52e5f4191222014aace
Dang Chuong Pham <dangchuong1999@outlook.com>
2021-04-28 22:55:57 +0300

    feat hero.service: Piped operators to the http call returning observable for logging, and handle errors.

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from 'src/app/hero';
 import { HEROES } from 'src/app/mock-heroes';
@@ -10,7 +11,7 @@ import { MessageService } from 'src/app/services/message.service';
   providedIn: 'root'
 })
 export class HeroService {
-  private heroesUrl = `/api/heroes`;
+  private baseUrl = `/api/heroes`;
 
   constructor(
     private messageService: MessageService,
@@ -18,24 +19,34 @@ export class HeroService {
   ) { }
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find(hero => {
-      return hero.id === id;
-    })
-
-    if (hero) {
-      this.logger(`Fetched hero id=${id}.`);
-      return of(hero);
-    } else {
-      throw Error('Hero not found.');
-    }
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap((_) => {
+        this.logger(`Fetched hero id=${id}.`);
+      }),
+      catchError(this.handleError<Hero>('getHero'))
+    )
   }
 
   getHeroes(): Observable<Hero[]> {
-    this.logger('Fetched heroes.');
-    return this.http.get<Hero[]>(this.heroesUrl);
+    return this.http.get<Hero[]>(this.baseUrl).pipe(
+      tap((_) => {
+        this.logger('Fetched heroes.'); // executes along with the observable
+      }),
+      catchError(this.handleError<Hero[]>('getHeroes', []))); // intercepts error only
   }
 
   private logger(message: string) {
     this.messageService.add(`[HeroService] ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); //TODO: send error to remote logging infra instead of to the console
+
+      this.logger(`Operation ${operation} failed: ${error.message}.`); //TODO: better message for end-user
+
+      return of(result as T); // return empty / default result to keep app running
+    }
   }
 }
